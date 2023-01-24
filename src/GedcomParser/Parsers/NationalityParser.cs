9336924ba289be4df1789fb28db91b1e8f37fbd0
Nikolay Zhivotenko <niko.zvt@gmail.com>
2023-01-24 16:19:19 +0300

    Added Nationality and Religion entities

---
+++ src/GedcomParser/Parsers/NationalityParser.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using GedcomParser.Entities;
+using GedcomParser.Entities.Internal;
+
+
+namespace GedcomParser.Parsers
+{
+    public static class NationalityParser
+    {
+        internal static Nationality ParseNationality(this ResultContainer resultContainer, GedcomChunk incomingChunk)
+        {
+            var nationality = new Nationality();
+            nationality.Id = incomingChunk.Id;
+            nationality.Name = incomingChunk.Data;
+
+            foreach (var chunk in incomingChunk.SubChunks)
+            {
+                switch (chunk.Type)
+                {
+                    case "PLAC":
+                    case "DATE":
+                        nationality.DatePlace = resultContainer.ParseDatePlace(chunk);
+                        break;
+
+                    case "SOUR":
+                        nationality.Citations.Add(resultContainer.ParseCitation(chunk));
+                        break;
+
+                    case "TYPE":
+                        nationality.Type = resultContainer.ParseText(chunk.Data, chunk);
+                        break;
+
+                    case "NOTE":
+                        nationality.Notes.Add(resultContainer.ParseNote(chunk.Data, chunk));
+                        break;
+
+                    default:
+                        resultContainer.Errors.Add($"Failed to handle Nationality Type='{chunk.Type}'");
+                        break;
+                }
+            }
+
+            return nationality;
+        }
+    }
+}
